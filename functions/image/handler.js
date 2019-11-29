09ac2b6e158f3c109855c28344759c067422f40c
GabrielGaray <leandro.garay@alumnos.frm.utn.edu.ar>
2019-11-28 23:54:27 -0300

    end image

--- functions/image/handler.js
+++ functions/image/handler.js
@@ -1,12 +1,36 @@
-"use strict"
+"use strict";
+const mongo = require("mongodb").MongoClient;
 
-module.exports = (event, context) => {
-    let err;
-    const result =             {
-        status: "You said: " + JSON.stringify(event.body)
+const mongoConnection = new Promise((resolve, reject) =>
+  mongo.connect(
+    "mongodb://mongodb:27017",
+    { useNewUrlParser: true, useUnifiedTopology: true },
+    (err, db) => (err ? reject(err) : resolve(db))
+  )
+);
+
+module.exports = async (req, res) => {
+  try {
+    const dbConnection = await mongoConnection;
+    const {
+      query: { size }
+    } = req;
+
+    const filter = {
+      S: { $or: [{ heigth: { $lte: 500 } }, { width: { $lte: 500 } }] },
+      M: { $or: [{ heigth: { $gt: 500 } }, { width: { $gt: 500 } }] },
+      L: { $and: [{ heigth: { $gt: 100 } }, { width: { $gt: 100 } }] }
     };
 
-    context
-        .status(200)
-        .succeed(result);
-}
+    const listFile = await dbConnection
+      .db("tp5")
+      .collection("files")
+      .find(filter[size])
+      .sort({ name: 1 })
+      .toArray();
+    res.status(200).succeed(JSON.stringify(listFile));
+  } catch (error) {
+    console.log("TCL: error", error);
+    res.status(400).fail(error);
+  }
+};
